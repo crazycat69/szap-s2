9aea45e50959fbed23e5daa51c7f81f58677c00b
Igor M. Liplianin <liplianin@me.by>
2008-09-30 12:56:31 +0300

    Implement NBC_QPSK modulation for DVB-S2

--- szap-s2.c
+++ szap-s2.c
@@ -108,7 +108,7 @@ static struct t_channel_parameter_map modulation_values[] = {
 //  {1024, DVBFE_MOD_QAM1024, "QAM1024" },
 //  {   1, DVBFE_MOD_BPSK,    "BPSK" },
   {   2, QPSK,    "QPSK" },
-//  {   3, DVBFE_MOD_OQPSK,   "OQPSK" },
+  {   3, NBC_QPSK,   "NBC_QPSK" },
   {   5, _8PSK,    "8PSK" },
   {   6, _16APSK,  "16APSK" },
 //  {   7, DVBFE_MOD_32APSK,  "32APSK" },
@@ -202,7 +202,7 @@ static char *usage_str =
     "     -p        : add pat and pmt to TS recording (implies -r)\n"
     "                 or -n numbers for zapping\n"
     "     -S        : delivery system type DVB-S=0, DVB-S2=1\n"
-    "     -M        : modulation 1=BPSK 2=QPSK 3=OQPSK 5=8PSK\n"
+    "     -M        : modulation 1=BPSK 2=QPSK 3=NBC-QPSK(for DVB-S2) 5=8PSK\n"
     "     -C        : fec 0=NONE 12=1/2 23=2/3 34=3/4 35=3/5 45=4/5 56=5/6 67=6/7 89=8/9 910=9/10 999=AUTO\n"
     "     -O        : rolloff 35=0.35 25=0.25 20=0.20 0=UNKNOWN\n";
 
